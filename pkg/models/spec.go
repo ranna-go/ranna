a6f3dbd7d3ac8c9ac6e8b69541ca48b579150bed
Ringo Hoffmann <riho@live.de>
2021-04-25 23:46:59 +0200

    add use keyword to spec [fix #6]

--- pkg/models/spec.go
+++ pkg/models/spec.go
@@ -7,17 +7,30 @@ type Spec struct {
 	FileName   string `json:"filename" yaml:"filename"`
 	Cmd        string `json:"cmd,omitempty" yaml:"cmd,omitempty"`
 	Registry   string `json:"registry,omitempty" yaml:"registry,omitempty"`
+	Use        string `json:"use,omitempty" yaml:"use,omitempty"`
 }
 
 // SpecMap wraps a map[string]*Spec.
 type SpecMap map[string]*Spec
 
-func (m SpecMap) Get(key string) (s Spec, ok bool) {
+func (m SpecMap) get(key string, isAlias bool) (s Spec, ok bool) {
 	sp, ok := m[key]
 	if !ok {
 		return
 	}
 
+	if sp.Use != "" {
+		if isAlias {
+			ok = false
+			return
+		}
+		return m.get(sp.Use, true)
+	}
+
 	s = *sp
 	return
+}
+
+func (m SpecMap) Get(key string) (Spec, bool) {
+	return m.get(key, false)
 }
