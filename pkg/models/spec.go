a3eb95b6a2bc0b513ea6bd16b0f57aaa2add2250
Ringo Hoffmann <riho@live.de>
2021-04-26 23:40:13 +0200

    hide unnecessary properties from spec response

--- pkg/models/spec.go
+++ pkg/models/spec.go
@@ -2,9 +2,9 @@ package models
 
 // Spec defines a code environment specification.
 type Spec struct {
-	Image      string `json:"image" yaml:"image"`
-	Entrypoint string `json:"entrypoint" yaml:"entrypoint"`
-	FileName   string `json:"filename" yaml:"filename"`
+	Image      string `json:"image,omitempty" yaml:"image,omitempty"`
+	Entrypoint string `json:"entrypoint,omitempty" yaml:"entrypoint,omitempty"`
+	FileName   string `json:"filename,omitempty" yaml:"filename,omitempty"`
 	Cmd        string `json:"cmd,omitempty" yaml:"cmd,omitempty"`
 	Registry   string `json:"registry,omitempty" yaml:"registry,omitempty"`
 	Use        string `json:"use,omitempty" yaml:"use,omitempty"`
