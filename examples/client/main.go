67cb7f2f23fca415049b876b29dfeac94c68c4e7
Lukas Schulte Pelkum <kbrt@protonmail.com>
2022-01-17 21:39:16 +0100

    update client example

--- examples/client/main.go
+++ examples/client/main.go
@@ -31,12 +31,28 @@ func main() {
 	must(err)
 
 	fmt.Println(c.Spec())
+
+	// This is a normal code execution
 	fmt.Println(c.Exec(models.ExecutionRequest{
-		Language:  "python3",
-		Code:      code,
-		Arguments: []string{"these", "are", "some", "args", "man"},
+		Language:         "python3",
+		Code:             code,
+		InlineExpression: false,
+		Arguments:        []string{"these", "are", "some", "args", "man"},
 		Environment: map[string]string{
 			"TEST": "some stuff over here",
 		},
+	}))
+
+	// ranna also supports inline expressions to run code without having to provide the boilerplate code around it
+	// (package declaration, main function)
+	//
+	// It is important to know though that this feature is not supported by all languages and the packages which are
+	// imported by default are limited. Please have a look at the language specification of your ranna instance.
+	fmt.Println(c.Exec(models.ExecutionRequest{
+		Language:         "go",
+		Code:             `fmt.Println("Hello, world!")`, // Note how this is only one small expression
+		InlineExpression: true,
+		Arguments:        []string{},
+		Environment:      map[string]string{},
 	}))
 }
