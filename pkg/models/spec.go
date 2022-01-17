69cc8939d6c5421b497157eb4f78618fd0fc6620
Lukas Schulte Pelkum <kbrt@protonmail.com>
2022-01-17 20:56:46 +0100

    add template field to spec model

--- pkg/models/spec.go
+++ pkg/models/spec.go
@@ -1,5 +1,7 @@
 package models
 
+import "strings"
+
 // Spec defines a code environment specification.
 type Spec struct {
 	Image      string `json:"image,omitempty" yaml:"image,omitempty"`
@@ -9,6 +11,12 @@ type Spec struct {
 	Registry   string `json:"registry,omitempty" yaml:"registry,omitempty"`
 	Use        string `json:"use,omitempty" yaml:"use,omitempty"`
 	Example    string `json:"example,omitempty" yaml:"example,omitempty"`
+	Template   string `json:"template,omitempty" yaml:"template,omitempty"`
+}
+
+// SupportsTemplating checks if a spec supports templating for inline expressions
+func (spec *Spec) SupportsTemplating() bool {
+	return spec.Template != "" && strings.Contains(spec.Template, "$${CODE}")
 }
 
 // SpecMap wraps a map[string]*Spec.
